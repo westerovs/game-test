cee262263b4b07144186f0fca413cc5faf034d7c
westerovs@mail.ru <westerovs@mail.ru>
2026-03-26 21:20:27 +0300

    refactoring

--- src/scripts/components/previewTemplate.js
+++ src/scripts/components/previewTemplate.js
@@ -2,8 +2,10 @@ export const previewTemplate = () => {
   return `
     <div class="preview">
       <div class="preview__title"></div>
-      <img class="preview__image" alt="preview"  src="#"/>
-      <div class="preview__story"></div>
+      <div class="preview__container">
+         <img class="preview__image" alt="preview"  src="#"/>
+        <div class="preview__story"></div>
+      </div>
     </div>
   `
 }
